4b484803affb687409a777c631402c03c88e01c4
Michel Lima <michelpl@yahoo.com.br>
2024-05-29 19:29:10 -0300

    :sparkles: Add mood field custom component

--- webapp/src/src/components/MoneysDataGrid/MoneysDataGrid.js
+++ webapp/src/src/components/MoneysDataGrid/MoneysDataGrid.js
@@ -4,7 +4,18 @@ import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the 
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 import ArgonTypography from "../Argon/ArgonTypography";
 import ArgonBox from "../Argon/ArgonBox";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import MoodEditor from "./MoodEditor";
+
+const MoodRenderer = (props) => {
+  const imageForMood = mood => 'https://www.ag-grid.com/example-assets/smileys/' + (mood === 'Happy' ? 'happy.png' : 'sad.png');
+
+  const mood = useMemo(() => imageForMood(props.value), [props.value]);
+
+  return (
+    <div className="mood-renderer"><img width="20px" src={mood}/></div>
+  );
+}
 
 export default function MoneysDataGrid() {
   const [rowData, setRowData] = useState([
@@ -12,6 +23,14 @@ export default function MoneysDataGrid() {
     { make: "Ford", model: "F-Series", price: 33850, electric: false, date: new Date() },
     { make: "Toyota", model: "Corolla", price: 29600, electric: false, date: new Date() },
   ]);
+
+  const defaultColDef = useMemo(() => {
+    return {
+      editable: true,
+      flex: 1,
+      minWidth: 100,
+    };
+  }, []);
 
   // Column Definitions: Defines the columns to be displayed.
   const [colDefs, setColDefs] = useState([
@@ -39,6 +58,14 @@ export default function MoneysDataGrid() {
       },
     },
     { field: "categories" },
+    {
+      field: "mood",
+      headerName: "Custom Mood",
+      cellRenderer: MoodRenderer,
+      cellEditor: MoodEditor,
+      cellEditorPopup: true,
+      editable: true,
+    },
   ]);
   return (<>
       <div
@@ -48,6 +75,8 @@ export default function MoneysDataGrid() {
         <AgGridReact
           rowData={rowData}
           columnDefs={colDefs}
+          reactiveCustomComponents={true}
+          defaultColDef={defaultColDef}
         />
       </div>
   </>)
