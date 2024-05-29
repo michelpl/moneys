65edc50e34d1323f379f33824ae4f733c17a86e3
Michel Lima <michelpl@yahoo.com.br>
2024-05-29 19:11:07 -0300

    :sparkles: Add date field

--- webapp/src/src/components/MoneysDataGrid/MoneysDataGrid.js
+++ webapp/src/src/components/MoneysDataGrid/MoneysDataGrid.js
@@ -8,17 +8,37 @@ import { useState } from "react";
 
 export default function MoneysDataGrid() {
   const [rowData, setRowData] = useState([
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true, date: new Date('12/01/2022') },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false, date: new Date() },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false, date: new Date() },
   ]);
 
   // Column Definitions: Defines the columns to be displayed.
   const [colDefs, setColDefs] = useState([
-    { field: "make" },
+    {
+      field: "make",
+      editable: true,
+    },
     { field: "model" },
     { field: "price" },
-    { field: "electric" }
+    {
+      field: "electric",
+      editable: true,
+    },
+    {
+      field: "date" ,
+      cellEditor: "agDateCellEditor",
+      editable:true,
+      valueFormatter: (params) => {
+        if (!params.value) {
+          return "";
+        }
+        const month = params.value.getMonth() + 1;
+        const day = params.value.getDate();
+        return `${day < 10 ? "0" + day : day}/${month < 10 ? "0" + month : month}/${params.value.getFullYear()}`;
+      },
+    },
+    { field: "categories" },
   ]);
   return (<>
       <div
